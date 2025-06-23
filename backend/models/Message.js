2c5e087c26205ab7eaa7085298d615947f281fac
hrithik18k <hrithikvasanthram@gmail.com>
2025-06-23 14:05:09 +0530

    ChatFix

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -1,8 +1,8 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 const messageSchema = new mongoose.Schema({
-    from:{type:mongoose.Schema.Types.ObjectId,ref:"User",Required:true},
-    to:{type:mongoose.Schema.Types.ObjectId, ref:"user", Required:true},
-    content:{type:Date, default:Date.now}
-})
-
-module.exports = mongoose.model("Message",messageSchema);
+    from: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    to: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    content: { type: String, required: true }, 
+    timestamp: { type: Date, default: Date.now }
+});
+module.exports = mongoose.model("Message", messageSchema);
