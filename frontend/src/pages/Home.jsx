7002099a25a114a33fe131ae7f22a2e883e75ce0
hrithik18k <hrithikvasanthram@gmail.com>
2026-02-14 15:17:22 +0530

    Hotfix: Added missing Badge import to Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
-import { Button } from "../components/ui";
+import { Button, Badge } from "../components/ui";
 
 // --- Components for Sections ---
 
